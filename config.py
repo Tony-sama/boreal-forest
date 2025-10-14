1ebd166d3b767b6c284aeab72cee5b8b4a214589
Tony Ribeiro <tonyribeiro.research.aca@gmail.com>
2025-10-14 17:16:49 +0200

    Start influence extraction pipeline

--- config.py
+++ config.py
@@ -1,4 +1,29 @@
 CHARCOAL_ID = "M15"
-TARGET = "Abies"
+TARGET = "Poaceae"
 
-ENCODE_CHANGE = True
+PROCESSED_DATA_PATH = "tmp/data_processed.csv"
+
+DISCARD_POLLEN = ['Abies',
+                  "Tsuga",
+                  "Acer",
+                  "Carpinus",
+                  "Corylus",
+                  "Nyssa",
+                  "Tilia",
+                  "Agoseris",
+                  "Brassicaceae",
+                  "Carya",
+                  "Epilobium",
+                  "Ericaceae",
+                  "Galium",
+                  "Lycopod",
+                  "Phytolithe",
+                  "Rosaceae",
+                  "Thalictrum",
+                  "Spore monolete",
+                  "Spore trilete",
+                  "Sarcobatus"]
+
+DISCRETIZATION_TRESHOLDS = [-0.01,0.01]
+
+DEBUG = True
