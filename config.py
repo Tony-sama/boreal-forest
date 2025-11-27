100cd25cbe61ab7c84155abca7c3636082ec3c5a
Tony Ribeiro <tonyribeiro.research.aca@gmail.com>
2025-11-27 18:48:46 +0100

    Update data encoding and lfit influence extraction to get all features influences into one graph.

--- config.py
+++ config.py
@@ -1,7 +1,61 @@
+THREADS = 32
+MIN_INFLUENCE_SUPPORT = 10
+
 CHARCOAL_ID = "M15"
-TARGET = "Synth_3"
 
 PROCESSED_DATA_PATH = "tmp/data_processed.csv"
+
+
+POACEAE_FEATURES = ['M15',
+            #'Juniperus',
+            #'Larix',
+            'Picea',
+            'Pinus',
+            #'Alnus',
+            'Betula',
+            #'Fraxinus',
+            #'Populus',
+            'Quercus',
+            #'Salix',
+            'Ulmus',
+            'Ambrosia',
+            'Artemesia',
+            #'Asteraceae',
+            'Chenopodiaceae',
+            'Cyperaceae',
+            #'Myrica',
+            'Poaceae',
+            #'Typha',
+            #'Aquatics'
+            ]
+
+FEATURES = ['M15',
+            'Juniperus',
+            'Larix',
+            'Picea',
+            'Pinus',
+            'Alnus',
+            'Betula',
+            'Fraxinus',
+            'Populus',
+            'Quercus',
+            'Salix',
+            'Ulmus',
+            'Ambrosia',
+            'Artemesia',
+            'Asteraceae',
+            'Chenopodiaceae',
+            'Cyperaceae',
+            'Myrica',
+            'Poaceae',
+            'Typha',
+            'Aquatics'
+            ]
+
+
+FEATURES = POACEAE_FEATURES
+
+TARGETS = FEATURES
 
 DISCARD_POLLEN = ['Abies',
                   "Tsuga",
@@ -28,4 +82,4 @@ DISCRETIZATION_TRESHOLDS = [0]
 
 MIN_SUPPORT = 1
 
-DEBUG = True
+DEBUG = False
